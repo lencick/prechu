afedfd4d0260ee0fd337c0e0515fa6fe8bd26e6f
Lena <lencick@inbox.ru>
2017-07-03 19:23:52 +0300

    header(top_line and slider), add fonts, img and owl

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    
+    $(".slider").owlCarousel({
+		items : 1,
+		nav : true,
+		navText : " ",
+		loop : true,
+		autoplay : true,
+		autoplayHoverPause : true,
+		fluidSpeed : 800,
+		autoplaySpeed : 800,
+		navSpeed : 800,
+		dotsSpeed : 800,
+		dragEndSpeed : 800
+	});
 });
