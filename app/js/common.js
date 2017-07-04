53a2b6eac5ffd50b41c02d7cd03cd0477f1da6d7
Lena <lencick@inbox.ru>
2017-07-04 12:29:01 +0300

    Toggle_menu, fix bag with logo(add fonts SourceSansPro)

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+
+	$(".toggle-mnu").click(function() {
+		$(this).toggleClass("on");
+		$("nav").slideToggle();
+	});
+	
     $(".slider").owlCarousel({
 		items : 1,
 		nav : true,
